a30c8ae41b06866dd18308a91a1c28f827b31fbe
John-David Basson <hire@johndavidbasson.com>
2025-05-18 13:18:13 -0500

    updating Models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :project
+  has_many :time_entries, dependent: :destroy
 end
--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,2 +1,10 @@
 class TimeEntry < ApplicationRecord
+  belongs_to :user
+  belongs_to :task
+
+  validates :start_time, :end_time, presence: true
+
+  def duration
+    end_time - start_time
+  end
 end
