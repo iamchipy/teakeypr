e5199d6d20c8671adda3e272ddc4f2aedead542d
John-David Basson <hire@johndavidbasson.com>
2025-05-18 18:29:36 -0500

    Projects ctr and view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,49 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!  # Ensure the user is authenticated
+
+  def index
+    @projects = current_user.projects  # Only show the current user's projects
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = current_user.projects.new(project_params)
+    if @project.save
+      redirect_to project_path(@project), notice: "Project successfully created!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to project_path(@project), notice: "Project successfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: "Project successfully deleted."
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
 end
