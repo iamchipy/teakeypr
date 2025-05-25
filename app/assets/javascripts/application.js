3b8a3e192c5c7fdf763d60bebd044f85f3d2a8cb
John-David Basson <hire@johndavidbasson.com>
2025-05-24 22:49:52 -0500

    Working model of multi-select search

--- app/assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require select2
-//= require time_entries
-//= require_tree .
-console.warn("app assets javascripts application.js LOADED!")
-console.log(window.jQuery);
-console.log(window.$);
--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -1,9 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
-import $ from "jquery"
-import "select2"
 
 export default class extends Controller {
     connect() {
-        $(this.element).select2()
+        console.log("🟢 Select2 controller connected");
+
+        if (typeof window.$ === "undefined" || typeof window.$.fn.select2 === "undefined") {
+            console.warn("Select2 not loaded yet. Retrying...");
+            setTimeout(() => this.connect(), 100);
+            return;
+        }
+
+        console.log("🟢 Initializing Select2 on:", this.element);
+        $(this.element).select2();
     }
 }
